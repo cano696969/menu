54f1457e7e8bf443e990ef1c31c9ab198bdf901e
Yair Cano Hernandez <158393938+cano696969@users.noreply.github.com>
2024-05-29 17:00:42 -0600

    Add files via upload

---
+++ qemu.sh
@@ -0,0 +1,18 @@
+
+
+sudo apt install virt-manager
+
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst libvirt-daemon
+
+sudo apt-get install libvirt-clients libvirt-daemon-system qemu-kvm bridge-utils dnsmasq
+
+sudo apt install cpu-checker -y
+
+
+
+sudo virsh list --all
+
+
+sudo virsh start vm_name
+
+sudo virsh console vm_name
